c9898e83916e2fdf359a9f0e0f93ca0e766db5ce
Chris Laprun <metacosm@gmail.com>
2021-05-04 21:59:15 +0200

    chore: add samples

---
+++ samples/spring-boot-auto-config/src/test/java/io/javaoperatorsdk/operator/sample/SpringBootStarterSampleApplicationIT.java
@@ -0,0 +1,11 @@
+package io.javaoperatorsdk.operator.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootStarterSampleApplicationIT {
+
+  @Test
+  void contextLoads() {}
+}
