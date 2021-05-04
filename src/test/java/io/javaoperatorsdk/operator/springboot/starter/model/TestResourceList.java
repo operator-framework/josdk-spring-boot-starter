8fa1430ff77ede4744f1cd80177a4eb4cf71df4f
Chris Laprun <metacosm@gmail.com>
2021-05-04 16:06:46 +0200

    feat: initial import from java-operator-sdk project

---
+++ src/test/java/io/javaoperatorsdk/operator/springboot/starter/model/TestResourceList.java
@@ -0,0 +1,5 @@
+package io.javaoperatorsdk.operator.springboot.starter.model;
+
+import io.fabric8.kubernetes.client.CustomResourceList;
+
+public class TestResourceList extends CustomResourceList<TestResource> {}
