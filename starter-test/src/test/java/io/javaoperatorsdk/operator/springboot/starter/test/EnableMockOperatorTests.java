21964e8488e939aed211d0011b87e72097883d7e
Nicholas Zhan <zhan_nicholas@outlook.com>
2022-06-15 22:07:48 +0800

    fix: incompatible fabric8-client version with operator-framework-core.

--- starter-test/src/test/java/io/javaoperatorsdk/operator/springboot/starter/test/EnableMockOperatorTests.java
+++ starter-test/src/test/java/io/javaoperatorsdk/operator/springboot/starter/test/EnableMockOperatorTests.java
@@ -1,18 +1,34 @@
 package io.javaoperatorsdk.operator.springboot.starter.test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Primary;
 
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.sample.CustomService;
+import io.javaoperatorsdk.operator.sample.CustomServiceReconciler;
+import io.javaoperatorsdk.operator.sample.ServiceSpec;
+import io.javaoperatorsdk.operator.springboot.starter.OperatorConfigurationProperties;
+import io.javaoperatorsdk.operator.springboot.starter.ReconcilerProperties;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @JsonTest
 @EnableMockOperator(crdPaths = "classpath:crd.yml")
@@ -45,6 +61,52 @@ class EnableMockOperatorTests {
                 .isNotNull();
   }
 
+  @Autowired
+  private OperatorConfigurationProperties operatorConfigurationProperties;
+
+  @Test
+  void customServiceReconciler() {
+    ReconcilerProperties properties =
+        operatorConfigurationProperties.getReconcilers().get("customservicereconciler");
+    assertNotNull(properties);
+    String testNS = "test-ns";
+    assertTrue(properties.getNamespaces().contains(testNS));
+    // create a namespace for testing
+    client.namespaces().create(namespace(testNS));
+    assertNotNull(client.namespaces().withName(testNS).get());
+    // create a CR
+    client.resources(CustomService.class).inNamespace(testNS)
+        .create(customService("test-name", "test-label"));
+    assertNotNull(client.resources(CustomService.class).inNamespace(testNS)
+        .withName("test-name").get());
+    // test if a service was created by the CustomServiceReconciler
+    await()
+        .atMost(15, TimeUnit.SECONDS)
+        .untilAsserted(
+            () -> assertNotNull(
+                client.resources(Service.class).inNamespace(testNS).withName("test-name").get()));
+  }
+
+  private Namespace namespace(String ns) {
+    return new NamespaceBuilder()
+        .withMetadata(
+            new ObjectMetaBuilder().withName(ns).build())
+        .build();
+  }
+
+  private CustomService customService(String name, String label) {
+    CustomService resource = new CustomService();
+    resource.setMetadata(
+        new ObjectMetaBuilder()
+            .withName(name)
+            .withResourceVersion("v1")
+            .build());
+    resource.setSpec(new ServiceSpec());
+    resource.getSpec().setName(name);
+    resource.getSpec().setLabel(label);
+    return resource;
+  }
+
   @SpringBootApplication
   @ComponentScan(
       includeFilters = {
@@ -54,5 +116,11 @@ class EnableMockOperatorTests {
           "io.javaoperatorsdk"
       })
   static class SpringBootTestApplication {
+    // Need to override the CustomServiceReconciler Bean to set KubernetesClient to the mocked one.
+    @Bean
+    @Primary
+    CustomServiceReconciler customServiceReconciler(KubernetesClient kubernetesClient) {
+      return new CustomServiceReconciler(kubernetesClient);
+    }
   }
 }
