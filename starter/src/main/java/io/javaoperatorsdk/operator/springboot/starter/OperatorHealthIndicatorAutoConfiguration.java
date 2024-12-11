e9de56eab9e4f4be8cffb2b7e4e9393bed7abf8b
ebma16 <martin.ebenschwanger@web.de>
2024-12-11 11:27:29 +0100

    chore: update to SpringBoot v3.4.0 and related fixes

--- starter/src/main/java/io/javaoperatorsdk/operator/springboot/starter/OperatorHealthIndicatorAutoConfiguration.java
+++ starter/src/main/java/io/javaoperatorsdk/operator/springboot/starter/OperatorHealthIndicatorAutoConfiguration.java
@@ -2,14 +2,14 @@ package io.javaoperatorsdk.operator.springboot.starter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import io.javaoperatorsdk.operator.Operator;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnBean(Operator.class)
-@ComponentScan("io.javaoperatorsdk.operator.springboot.starter")
+@Import(OperatorAutoConfiguration.class)
 public class OperatorHealthIndicatorAutoConfiguration {
 
   @Bean
